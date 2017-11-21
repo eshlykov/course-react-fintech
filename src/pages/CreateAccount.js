592616cb771f7eccd32c1bfc3cc1f68fa95372e5
eshlykov <e.a.shlykov@gmail.com>
2017-11-21 18:37:17 +0300

    Поля в форме

--- src/pages/CreateAccount.js
+++ src/pages/CreateAccount.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div>
-    <h2>Создание аккаунта</h2>
-    <p>Здесь мы будем создавать новые счета</p>
-  </div>
-);
+class CreateAccount extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      currency: '',
+      description: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <input name="name" type="text" value={this.state.name} onChange={this.handleChange} />
+        <input name="currency" type="text" value={this.state.currency} onChange={this.handleChange} />
+        <input name="description" type="text" value={this.state.description} onChange={this.handleChange} />
+      </form>
+    );
+  }
+}
+
+export default CreateAccount;
