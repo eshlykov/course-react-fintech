dae3851cce952bc3e93fe777705c58269774c736
eshlykov <e.a.shlykov@gmail.com>
2017-11-21 19:50:34 +0300

    Пофиксил сайдбар на случай без аккаунтов

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -9,6 +9,19 @@ import './Sidebar.css';
 
 class Sidebar extends  React.Component {
   render() {
+    const accountsList = (this.props.accounts ? Object.entries(this.props.accounts) : [])
+      .map(([index, account]) => (
+      <NavLink to={`/account/${index}`} className='Link' activeClassName='Link--active' key={`account-${index}`}>
+        <div className='Sidebar__account'>
+          <div className='Sidebar__account-name'>
+            {account.name}
+          </div>
+          <div className='Sidebar__account-amount'>
+            <Money value={account.amount} currency={account.currency} />
+          </div>
+        </div>
+      </NavLink>
+    ));
 
     return (
       <div className='Sidebar'>
@@ -16,18 +29,7 @@ class Sidebar extends  React.Component {
           Счета
         </div>
 
-        {Object.entries(this.props.accounts).map(([index, account]) => (
-          <NavLink to={`/account/${index}`} className='Link' activeClassName='Link--active' key={`account-${index}`}>
-            <div className='Sidebar__account'>
-              <div className='Sidebar__account-name'>
-                {account.name}
-              </div>
-              <div className='Sidebar__account-amount'>
-                <Money value={account.amount} currency={account.currency} />
-              </div>
-            </div>
-          </NavLink>
-        ))}
+        {accountsList}
 
         <NavLink to='/create-account' className='Link' activeClassName='Link--active'>
           <div className='Sidebar__account'>
